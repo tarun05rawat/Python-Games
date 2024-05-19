a527933c0ae3891651f9638f88c947af46291893
Tarun Rawat <144971861+tarun05rawat@users.noreply.github.com>
2024-05-19 01:43:42 -0400

    Update main.py

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -5,65 +5,60 @@ from scoreboard import Scoreboard
 import time
 import random
 
-def main():
-  screen = Screen()
-  screen.setup(width=600,height=600)
-  screen.bgcolor("black")
-  screen.title("Tarun's Snake Game")
-  screen.tracer(0)
+
+screen = Screen()
+screen.setup(width=600,height=600)
+screen.bgcolor("black")
+screen.title("Tarun's Snake Game")
+screen.tracer(0)
 
 
-  snake = Snake()
-  food = Food()
-  scoreboard = Scoreboard()
-
-
-
-
-
-  screen.listen()
-  screen.onkey(snake.up, "Up")
-  screen.onkey(snake.down, "Down")
-  screen.onkey(snake.left, "Left")
-  screen.onkey(snake.right, "Right")
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 
 
-    
 
-  game_is_on = True
 
-  while game_is_on:
-    screen.update()
-    time.sleep(0.09)
-    snake.move()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-    if snake.head.distance(food) < 15:      #DETECT COLLISION WITH FOOD
-      food.refresh()
-      snake.extend()
-      scoreboard.newscore()
-    
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:        # DETECT COLLISION WITH WALLS
-      game_is_on = False
-      scoreboard.game_over()
-    
-    for segment in snake.segments:              # DETECT COLLISION WITH TAIL
-      if segment == snake.head:
-        pass
-      elif snake.head.distance(segment) < 10:
-        game_is_on = False
-        scoreboard.game_over()
-      
+
+game_is_on = True
+
+
+while game_is_on:
   screen.update()
-  play_again = screen.textinput("Game Over", "Play again? Type Yes or No: ")
+  time.sleep(0.09)
+  snake.move()
 
-  if play_again.lower() == 'yes':
-    screen.clearscreen()
-    main()
-  else:
-    screen.bye()
+  if snake.head.distance(food) < 15:      #DETECT COLLISION WITH FOOD
+    food.refresh()
+    snake.extend()
+    scoreboard.newscore()
   
+  if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:        # DETECT COLLISION WITH WALLS
+    scoreboard.reset_score()
+    snake.reset_snake()
 
-  screen.exitonclick()
+  
+  for segment in snake.segments:              # DETECT COLLISION WITH TAIL
+    if segment == snake.head:
+      pass
+    elif snake.head.distance(segment) < 10:
+      scoreboard.reset()
+      snake.reset_snake()
 
-main()
+      
+
+      
+      
+    
+
+
+
+screen.exitonclick()
