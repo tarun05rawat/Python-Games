9b408a36921a951a9c0c1df38e6155ff5c5cecbf
Tarun Rawat <144971861+tarun05rawat@users.noreply.github.com>
2024-05-19 01:43:57 -0400

    Update snake.py

--- Snake Game/snake.py
+++ Snake Game/snake.py
@@ -15,6 +15,13 @@ class Snake(Turtle):
   def create_snake(self):
     for position in STARTING_POSITIONS:
       self.add_segment(position)
+
+  def reset_snake(self):
+    for seg in self.segments:
+      seg.goto(1000,1000)
+    self.segments.clear()
+    self.create_snake()
+    self.head = self.segments[0]
   
 
   def add_segment(self,position):
