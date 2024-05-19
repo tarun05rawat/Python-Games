b10561fde51e9811fa5c9c02cd7273d80fdd9a8f
Tarun Rawat <144971861+tarun05rawat@users.noreply.github.com>
2024-05-19 01:44:11 -0400

    Update scoreboard.py

--- Snake Game/scoreboard.py
+++ Snake Game/scoreboard.py
@@ -1,25 +1,40 @@
 from turtle import Turtle
 
+FONT = ('Courier', 24, 'normal')
+ALIGNMENT = "center"
+
+
 class Scoreboard(Turtle):
   def __init__(self):
     super().__init__()
     self.score = 0
+    with open("data.txt") as data:
+      self.highscore = int(data.read())
     self.color("white")
     self.penup()
     self.goto(0,270)
-    self.write(f"Scoreboard: {self.score}",move=False, align="center", font=('Courier', 24, 'normal'))
     self.hideturtle()
-    
+    self.update_scoreboard()
   
+  def update_scoreboard(self):
+    self.clear()
+    self.write(f"Score: {self.score} High Score: {self.highscore}", align=ALIGNMENT, font=FONT)
+  
+  def reset_score(self):
+    if self.score > self.highscore:
+      self.highscore = self.score
+    with open("data.txt",mode="w") as data:
+      data.write(f"{self.highscore}")
+
+    self.score = 0
+    self.update_scoreboard()
+    
   def newscore(self):
     self.score += 1
     self.clear()
-    self.write(f"Scoreboard: {self.score}",move=False, align="center", font=('Courier', 24, 'normal'))
-
-  def game_over(self):
-    self.goto(0,0)
-    self.write("Game Over!",move=False, align="center", font=('Courier', 30, 'normal'))
+    self.update_scoreboard()
   
+
 
     
 
